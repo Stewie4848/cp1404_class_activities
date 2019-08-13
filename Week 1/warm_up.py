2fcadf3bdaa6fcc61ccbc1fcf6e503561e258579
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-14 09:42:03 +1000

    Week 1 determining adulthood warm up

---
+++ Week 1/warm_up.py
@@ -0,0 +1,18 @@
+"""Warm up for 01
+Write pseudocode to ask the user for their age and tell them if they are an adult or child"""
+
+"""
+get age
+if age > 18
+    print adult
+else
+    print child
+"""
+
+print("Hello World")
+
+age = int(input("Age: "))
+if age >= 18:
+    print("Adult")
+else:
+    print("Child")
