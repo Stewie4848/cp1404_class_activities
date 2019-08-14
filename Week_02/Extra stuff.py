326f2d2c516952dcdc41abb586c8454add31c0b5
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-14 10:09:40 +1000

    Add Class Activity directory

---
+++ Week_02/Extra stuff.py
@@ -0,0 +1,20 @@
+# for i in range(1, 10, 4):
+#     print(i, i * 2, end=" ")
+
+# x = 5
+# for i in range(x):
+#     x = x + i
+#     print(x, end=" ")
+
+# nums1 = [1, -5, 2, 0, 4, 2, -3]
+# nums2 = [1, -5, 2, 4, 4, 2, 7]
+# result = 0
+# j = 0
+# while j < len(nums1):
+#     if nums1[j] != nums2[j]:
+#         result = result + 1
+#     j = j + 1
+#
+# print(result)
+
+print(7 % 3)
