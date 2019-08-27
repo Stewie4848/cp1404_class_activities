13c9d887ce398ddb300f6097182e99a68d88116a
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-08-28 09:36:20 +1000

    Add list exercise

---
+++ Week_04/lists_exercise.py
@@ -0,0 +1,20 @@
+# subjects = ['CP1404', 'CP1804', 'CP5632', 'MA1008']
+#  for subject in range(len(subjects)):
+#      print("{} - {}".format(subject + 1, subjects[subject]))
+#
+#  for i, subject in enumerate(subjects):
+#      print("{} - {}".format(i + 1, subject))
+#
+#  scores = []
+#  score = int(input("Score: "))
+#  while score >= 0:
+#      scores.append(score)
+#      score = int(input("Score: "))
+#  print("Your highest score is", max(scores))
+#  subjects = ['CP1404', 'CP1804', 'CP5632', 'MA1008']
+# cp_subjects = [subject for subject in subjects if subject.startswith("CP")]
+# year_numbers = [subject[2:] for subject in subjects]
+# print(year_numbers)
+
+text = "this is a sentence"
+three_letter_words = []
