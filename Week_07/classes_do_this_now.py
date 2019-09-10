8b731ee4378dad667ea04baf07847ba4d53bad01
Jack Stewart <jack.stewart1@my.jcu.edu.au>
2019-09-11 09:38:35 +1000

    finish do this now

--- Week_07/classes_do_this_now.py
+++ Week_07/classes_do_this_now.py
@@ -1,5 +1,16 @@
 class Plant:
-    def __init__(self, name="", height=0, growth_rate=0):
+    def __init__(self, name="", height=0.0, growth_rate=0):
         self.name = name
         self.height = height
         self.growth_rate = growth_rate
+
+    def __str__(self):
+        return "{}{}{}".format(self.name, self.height, self.growth_rate)
+
+    def water(self, water):
+        self.height += self.growth_rate * water
+
+
+p = Plant("Pigeon Pea", 0.3, 1.4)
+p.water(10)
+print(p)
